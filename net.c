8658bde7a3781702d9844f0b0e020a4f308be540
MayugeStudio <72333443+MayugeStudio@users.noreply.github.com>
2026-07-27 12:58:07 +0900

    Completion of step12

--- net.c
+++ net.c
@@ -6,6 +6,8 @@
 
 #include "util.h"
 #include "net.h"
+#include "arp.h"
+#include "ip.h"
 #include "icmp.h"
 
 struct net_protocol {
@@ -201,6 +203,10 @@ net_init(void)
     infof("initialize...");
     if (platform_init() == -1) {
         errorf("platform_init() failure");
+        return -1;
+    }
+    if (arp_init() == -1) {
+        errorf("arp_init() failure");
         return -1;
     }
     if (ip_init() == -1) {
